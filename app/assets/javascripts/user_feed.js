4aee912915f8a227c634a58dc2969098a2f6412b
Jonathan Luk <jluk87@gmail.com>
2019-09-10 14:56:57 -0400

    Groups can now be created from the index page with the submit_group_form() js function.

--- app/assets/javascripts/user_feed.js
+++ app/assets/javascripts/user_feed.js
@@ -6,9 +6,22 @@ $(document).ready(function() {
 
 function display_group_form() {
   $('#create_group_btn').click(function(event) {
+    event.preventDefault();
     let form = $.get('/groups/new')
     form.done(function(data) {
       $('#create_group_form').append(data)
     })
   });
+}
+
+function submit_group_form() {
+  $('#create_group_form').submit(function(event) {
+    const values = $(this).serialize();
+    console.log(values)
+    const creating_group = $.post('/groups', values);
+    creating_group.done(function(data) {
+      console.log(data)
+    })
+  });
+
 }
