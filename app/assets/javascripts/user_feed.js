cc2dfb693f63e62a3f2d86f18f719a3fae3e6b8f
Jonathan Luk <jluk87@gmail.com>
2019-09-10 15:48:12 -0400

    Fixed the submit_group_form() js function - it was submitting through Rails. Added a 'Cancel' button to close the form. Added listeners to the display_group_form js function.

--- app/assets/javascripts/user_feed.js
+++ app/assets/javascripts/user_feed.js
@@ -9,14 +9,30 @@ function display_group_form() {
     event.preventDefault();
     let form = $.get('/groups/new')
     form.done(function(data) {
-      $('#create_group_form').append(data)
-    })
+      $('#create_group_form').html(data)
+      attachFormListeners();
+    });
   });
 }
 
-function submit_group_form() {
-  $('#create_group_form').submit(function(event) {
+function attachFormListeners() {
+  cancelGroupFormListener();
+  submitGroupFormListener();
+}
+
+function cancelGroupFormListener() {
+  $('#cancel_group_form').click(function(event) {
+    event.preventDefault();
+    $('#create_group_form').empty();
+    window.scrollTo(0,0)
+  });
+}
+
+function submitGroupFormListener() {
+  $('form').submit(function(event) {
+    event.preventDefault();
     const values = $(this).serialize();
+    debugger;
     console.log(values)
     const creating_group = $.post('/groups', values);
     creating_group.done(function(data) {
