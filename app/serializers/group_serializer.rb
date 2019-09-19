b1f7273029ed5050fb7850128399786dd851e981
Jonathan Luk <jluk87@gmail.com>
2019-09-19 18:41:37 -0400

    Added current_member key to the group_data json response to identify whether the current user is an admin for the group.

--- app/serializers/group_serializer.rb
+++ app/serializers/group_serializer.rb
@@ -1,6 +1,6 @@
 class GroupSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
-  attributes :id, :name, :description, :group_image, :invitations, :assigned_tasks, :available_tasks, :recent_completed_tasks, :users, :errors, :members
+  attributes :id, :name, :description, :group_image, :invitations, :assigned_tasks, :available_tasks, :recent_completed_tasks, :current_member, :errors, :members
 
   def group_image
     rails_blob_path(object.image) if object.image.attachment
@@ -10,10 +10,15 @@ class GroupSerializer < ActiveModel::Serializer
     object.announcements.order({ created_at: :desc })
   end
 
+  def current_member
+    member = Membership.find_by(user_id: current_user.id)
+    { id: member.id, group_id: member.group_id, user_id: member.user_id, admin: member.admin }
+  end
+
   def members
     object.memberships.map do |member|
       user = User.find_by(id: member.user_id)
-      { id: member.id, group_id: member.group_id, name: user.name, admin: member.admin, avatar: rails_blob_path(user.avatar) }
+      { id: member.id, group_id: member.group_id, user_id: user.id, name: user.name, admin: member.admin, avatar: rails_blob_path(user.avatar) }
     end
   end
 
