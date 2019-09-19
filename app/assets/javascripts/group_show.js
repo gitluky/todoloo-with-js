5789c9b1aa84d7b177027e6532848a822ab6edff
Jonathan Luk <jluk87@gmail.com>
2019-09-19 19:23:51 -0400

    Created getGroupData() in group_show.js to get the json response from groups#group_data. Created an admin variable.

--- app/assets/javascripts/group_show.js
+++ app/assets/javascripts/group_show.js
@@ -2,9 +2,22 @@ $('.groups.show').ready(function() {
  getGroupData();
 });
 
+let admin = false
+
 function getGroupData() {
   const groupId = $('#group_show').attr('data-groupid');
   $.get('/groups/' + groupId + '/group_data', function(resp) {
-    console.log(resp)
+    if (resp['data']['attributes']['current-member']['admin'] === true) {
+      admin = true;
+    } else {
+      admin = false;
+    }
+    displayGroupMembers(resp);
   });
+}
+
+
+function displayGroupMembers(resp) {
+
+
 }
