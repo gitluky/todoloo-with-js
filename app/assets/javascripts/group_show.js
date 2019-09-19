8e30e3b54103d89e37a32d39bd5ea6d30d84b1b4
Jonathan Luk <jluk87@gmail.com>
2019-09-19 18:32:30 -0400

    Changed the serialization to display users through #memberships, which removed the need to admin and non-admin serializers.

--- app/assets/javascripts/group_show.js
+++ app/assets/javascripts/group_show.js
@@ -1,3 +1,10 @@
-$('.group.show').ready(function() {
-
+$('.groups.show').ready(function() {
+ getGroupData();
 });
+
+function getGroupData() {
+  const groupId = $('#group_show').attr('data-groupid');
+  $.get('/groups/' + groupId + '/group_data', function(resp) {
+    console.log(resp)
+  });
+}
--- app/serializers/group_serializer.rb
+++ app/serializers/group_serializer.rb
@@ -1,9 +1,6 @@
 class GroupSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
-  attributes :id, :name, :description, :group_image, :invitations, :assigned_tasks, :available_tasks, :recent_completed_tasks, :users, :errors
-
-  has_many :admins, serializer: GroupAdminSerializer
-  has_many :non_admins, serializer: GroupNonAdminSerializer
+  attributes :id, :name, :description, :group_image, :invitations, :assigned_tasks, :available_tasks, :recent_completed_tasks, :users, :errors, :members
 
   def group_image
     rails_blob_path(object.image) if object.image.attachment
@@ -11,6 +8,13 @@ class GroupSerializer < ActiveModel::Serializer
 
   def announcements_in_order
     object.announcements.order({ created_at: :desc })
+  end
+
+  def members
+    object.memberships.map do |member|
+      user = User.find_by(id: member.user_id)
+      { id: member.id, group_id: member.group_id, name: user.name, admin: member.admin, avatar: rails_blob_path(user.avatar) }
+    end
   end
 
 end
